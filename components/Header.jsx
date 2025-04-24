ff5253413f990f9195530b907d07fa26a86c6e20
Frozanziaee <frozanziaie234@gmail.com>
2025-04-24 09:03:09 +0430

    fix images

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,8 @@
+import images from '../images/globe.png'
 export default function Header() {
     return (
         <header>
-            <img src="../images/globe.png" alt="globe icon" />
+            <img src={images} />
             <h1>my travel journal.</h1>
         </header>
     )
