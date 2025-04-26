12b618bfe5412d8c2b7f069ebb263ed2a41d71a9
Frozanziaee <frozanziaie234@gmail.com>
2025-04-26 13:53:59 +0430

    update data

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ export default [
         title: "Kaaba",
         country: "Saudi Arabia",
         googleMapsLink: "https://www.google.com/maps/place/%DA%A9%D8%B9%D8%A8%D9%87%E2%80%AD/@21.4224779,39.8262775,18z/data=!3m1!4b1!4m6!3m5!1s0x15c204b74c28d467:0xb2f543a618225767!8m2!3d21.4225453!4d39.8261901!16zL20vMDFsaDJq?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "",
+        dates: "01 Jan, 2025 - 15 March, 2025",
         text: "Kaaba, shrine located near the center of the Great Mosque in Mecca, Saudi Arabia, and considered by Muslims everywhere to be the most sacred spot on Earth. Muslims orient themselves toward this small shrine during the five daily prayers, bury their dead facing its meridian, and cherish the ambition of visiting it on pilgrimage, or hajj, in accord with the command set out in the Qurʾān. "
     },
     
@@ -22,7 +22,7 @@ export default [
         title: "Eiffel Tower",
         country: "Paris",
         googleMapsLink: "https://www.google.com/maps/place/%D8%A8%D8%B1%D8%AC+%D8%A5%D9%8A%D9%81%D9%84%E2%80%AD/@48.8583701,2.2944813,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813!16zL20vMDJqODE?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: " 28 Jan, 1887 - 31 March 1889",
+        dates: "01 Jan, 2024 - 15 March, 2024",
         text: "Eiffel Tower, wrought-iron structure in Paris that is among the most famous landmarks in the world. It is also a technological masterpiece in building-construction history."
     },
 
@@ -35,7 +35,7 @@ export default [
         title: "Geirangerfjord",
         country: "Norway",
         googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
-        dates: "23 Feb, 1847 - 27 Feb, 2021",
+        dates: "01 Jan, 2024 - 15 March, 2024",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
     {
@@ -47,7 +47,7 @@ export default [
         title: "Hagia Sophia",
         country: "Torkey",
         googleMapsLink: "https://www.google.com/maps/place/41%C2%B000'30.6%22N+28%C2%B058'48.0%22E/@41.0085094,28.9799975,13z/data=!4m4!3m3!8m2!3d41.0085094!4d28.9799975?hl=en&entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "23 Feb, 1847 - 27 Feb, 1849",
+        dates: "01 Augest, 2025 - 15 Oct, 2025",
         text: "Hagia Sophia Grand Mosque (Turkish: Ayasofya-i Kebir Cami-i Şerifi; Ottoman Turkish: آیا صوفیاى کبیر جامع شریف), is a mosque, museum and former church serving as a major cultural and historical site in Istanbul, Turkey."
 
     },
@@ -60,7 +60,7 @@ export default [
         title: "Galata Tower",
         country: "Turkey, Istanbul",
         googleMapsLink: "https://www.google.com/maps/place/%D8%A8%D8%B1%D8%AC+%DA%AF%D8%A7%D9%84%D8%A7%D8%AA%D8%A7%E2%80%AD/@41.025569,28.9741286,17z/data=!3m1!4b1!4m6!3m5!1s0x14cab9e7a7777c43:0x4c76cf3dcc8b330b!8m2!3d41.025569!4d28.9741286!16zL20vMDFmaHhi?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "23 Feb, 1348 - 27 Feb, 1349",
+        dates: "01 Jan, 2023 - 15 April, 2023",
         text: "Galata Tower, a renowned landmark in Istanbul, stands as an enduring symbol of the city's rich history and breathtaking views.  "
     },
     {
@@ -72,7 +72,7 @@ export default [
         title: "Gull ghondi",
         country: "Afghanistan",
         googleMapsLink: "https://www.google.com/maps/place/%D8%B3%D8%B1%DA%A9+%DA%AF%D9%84%D8%BA%D9%86%D8%AF%DB%8C%D8%8C+%DA%86%D8%A7%D8%B1%DB%8C%DA%A9%D8%A7%D8%B1%E2%80%AD/@35.0149143,69.1577501,17z/data=!3m1!4b1!4m6!3m5!1s0x38d1916543d998ad:0xfc77bc12b738bce8!8m2!3d35.0149143!4d69.1577501!16s%2Fg%2F119v3sx7d?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "1 March - 31 April",
+        dates: "01 March, 2025 - 15 March, 2025",
         text: "GaGul Ghondi Hill is a stunningly beautiful place located in the west of Parwan province where visitors from all across the country come for picnic and to see the blossom of flowers called Arghawan.l "
     },
     {
@@ -84,7 +84,7 @@ export default [
         title: "Karabi",
         country: "Thailand",
         googleMapsLink: "https://www.google.com/maps/place/%DA%A9%D8%B1%D8%A7%D8%A8%DB%8C%D8%8C+%D8%AA%D8%A7%DB%8C%D9%84%D9%86%D8%AF%E2%80%AD/@8.0745583,99.0132952,9z/data=!3m1!4b1!4m6!3m5!1s0x30518d792d7f9309:0x10223bc2c364c90!8m2!3d8.1191811!4d99.1013498!16zL20vMDFnOW1m?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "30 Sep 2019",
+        dates: "01 Jan, 2023 - 15 June, 2023",
         text: "Krabi is abundant in scenic beauty with lush tropical greenery, rugged limestone cliffs, meandering caves and a stunning coastline with some of the best beaches in the world."
     },
     {
@@ -96,7 +96,7 @@ export default [
         title: "Band-e Amir ",
         country: "Afghanistan",
         googleMapsLink: "https://www.google.com/maps/place/%D9%BE%D8%A7%D8%B1%DA%A9+%D9%85%D9%84%DB%8C+%D8%A8%D9%86%D8%AF+%D8%A7%D9%85%DB%8C%D8%B1%E2%80%AD/@34.8353265,67.1810103,13z/data=!3m1!4b1!4m6!3m5!1s0x3f2d4f2bfcf94275:0xbc2a656ca9d5e72a!8m2!3d34.8424366!4d67.211793!16s%2Fg%2F11bwymr6v5?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "22 May 2009",
+        dates: "01 Jan, 2022 - 15 March, 2022",
         text: "Band-E-Amir is a naturally created group of lakes with special geological formations and structure, as well as natural and unique beauty. Depth is not known, the color is pure blue. For the attraction of the local and international tourists, Band-E-Amir is important, and is protected as a National Park of Afghanistan. "
     },
     {
@@ -108,7 +108,7 @@ export default [
         title: "Shahr-e Zuhak ",
         country: "Afghanistan",
         googleMapsLink: "https://www.google.com/maps/place/34%C2%B049'35.0%22N+67%C2%B049'18.0%22E/@34.8263889,67.8216667,13z/data=!4m4!3m3!8m2!3d34.8263889!4d67.8216667?hl=en&entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "500 - 600",
+        dates: "01 Jan, 2022 - 15 March, 2022",
         text: "Shahr-e Zuhak or Zuhak City, also known as The Red City, Ruins of a HUGE city sacked by Ghengis Khan. The architecture is beautiful, the views are unbelievable."
     },
     {
@@ -120,7 +120,7 @@ export default [
         title: "Taj Mahal",
         country: "India",
         googleMapsLink: "https://www.google.com/maps/place/%D8%AA%D8%A7%D8%AC+%D9%85%D8%AD%D9%84%E2%80%AD/@27.1751448,78.0421422,17z/data=!3m1!4b1!4m6!3m5!1s0x39747121d702ff6d:0xdd2ae4803f767dde!8m2!3d27.1751448!4d78.0421422!16zL20vMGw4Y2I?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
-        dates: "1631 - 1653",
+        dates: "01 Sep, 2021 - 15 Dec, 2021",
         text: "An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage."   
     }
 
