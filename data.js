5938b8a846e27eb95028ff3619ef6af7ed5608ac
Frozanziaee <frozanziaie234@gmail.com>
2025-04-26 12:22:27 +0430

    make 10 different places and update the style

--- data.js
+++ data.js
@@ -1,28 +1,31 @@
+
 export default [
     {
         id: 1,
         img: {
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-            alt: "Mount Fuji"
+            src: "https://cdn.britannica.com/43/156343-050-CD194769/pilgrims-Muslim-Kabah-Great-Mosque-of-Mecca.jpg?w=300",
+            alt: "Kaaba"
         },
-        title: "Mount Fuji",
-        country: "Japan",
-        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
-        dates: "12 Jan, 2021 - 24 Jan, 2021",
-        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        title: "Kaaba",
+        country: "Saudi Arabia",
+        googleMapsLink: "https://www.google.com/maps/place/%DA%A9%D8%B9%D8%A8%D9%87%E2%80%AD/@21.4224779,39.8262775,18z/data=!3m1!4b1!4m6!3m5!1s0x15c204b74c28d467:0xb2f543a618225767!8m2!3d21.4225453!4d39.8261901!16zL20vMDFsaDJq?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "",
+        text: "Kaaba, shrine located near the center of the Great Mosque in Mecca, Saudi Arabia, and considered by Muslims everywhere to be the most sacred spot on Earth. Muslims orient themselves toward this small shrine during the five daily prayers, bury their dead facing its meridian, and cherish the ambition of visiting it on pilgrimage, or hajj, in accord with the command set out in the Qurʾān. "
     },
+    
     {
         id: 2,
         img: {
-            src: "https://scrimba.com/links/travel-journal-australia-image-url",
-            alt: "Sydney Opera House"
+            src: 'https://cdn.britannica.com/52/245552-050-3D7334F9/Eiffel-Tower-Paris.jpg?w=300',
+            alt: "Eiffel Tower Paris"
         },
-        title: "Sydney Opera House",
-        country: "Australia",
-        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
-        dates: "27 May, 2021 - 8 Jun, 2021",
-        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+        title: "Eiffel Tower",
+        country: "Paris",
+        googleMapsLink: "https://www.google.com/maps/place/%D8%A8%D8%B1%D8%AC+%D8%A5%D9%8A%D9%81%D9%84%E2%80%AD/@48.8583701,2.2944813,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813!16zL20vMDJqODE?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: " 28 Jan, 1887 - 31 March 1889",
+        text: "Eiffel Tower, wrought-iron structure in Paris that is among the most famous landmarks in the world. It is also a technological masterpiece in building-construction history."
     },
+
     {
         id: 3,
         img: {
@@ -32,7 +35,93 @@ export default [
         title: "Geirangerfjord",
         country: "Norway",
         googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        dates: "23 Feb, 1847 - 27 Feb, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
+    {
+        d: 4,
+        img: {
+            src: "https://lh3.googleusercontent.com/gpms-cs-s/AIMqDu29wu87NnZHRMIhRAMsUFvb8E1TZ4_APwVlc98QlkIoR9VydAbufZi1HNToK2367mMjMCNGvNVsmBcgfpAf-9-BhZJtP52qlQBFJsvBtVMz6qLphFzNgNySYOz0WMDnAUsN9RKN=w400-h300-n-k-no",
+            alt: "Hagia Sophia"
+        },
+        title: "Hagia Sophia",
+        country: "Torkey",
+        googleMapsLink: "https://www.google.com/maps/place/41%C2%B000'30.6%22N+28%C2%B058'48.0%22E/@41.0085094,28.9799975,13z/data=!4m4!3m3!8m2!3d41.0085094!4d28.9799975?hl=en&entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "23 Feb, 1847 - 27 Feb, 1849",
+        text: "Hagia Sophia Grand Mosque (Turkish: Ayasofya-i Kebir Cami-i Şerifi; Ottoman Turkish: آیا صوفیاى کبیر جامع شریف), is a mosque, museum and former church serving as a major cultural and historical site in Istanbul, Turkey."
+
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://cdn.prod.website-files.com/65329423a966d94a21420905/674ed393aafc86e1dcd72804_galata-tower_7837.jpeg",
+            alt: "Galata Tower"
+        },
+        title: "Galata Tower",
+        country: "Turkey, Istanbul",
+        googleMapsLink: "https://www.google.com/maps/place/%D8%A8%D8%B1%D8%AC+%DA%AF%D8%A7%D9%84%D8%A7%D8%AA%D8%A7%E2%80%AD/@41.025569,28.9741286,17z/data=!3m1!4b1!4m6!3m5!1s0x14cab9e7a7777c43:0x4c76cf3dcc8b330b!8m2!3d41.025569!4d28.9741286!16zL20vMDFmaHhi?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "23 Feb, 1348 - 27 Feb, 1349",
+        text: "Galata Tower, a renowned landmark in Istanbul, stands as an enduring symbol of the city's rich history and breathtaking views.  "
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG-c_wDPAOegCeMiA4batLTcjztqadCejZAg&s",
+            alt: "Gul ghondi"
+        },
+        title: "Gull ghondi",
+        country: "Afghanistan",
+        googleMapsLink: "https://www.google.com/maps/place/%D8%B3%D8%B1%DA%A9+%DA%AF%D9%84%D8%BA%D9%86%D8%AF%DB%8C%D8%8C+%DA%86%D8%A7%D8%B1%DB%8C%DA%A9%D8%A7%D8%B1%E2%80%AD/@35.0149143,69.1577501,17z/data=!3m1!4b1!4m6!3m5!1s0x38d1916543d998ad:0xfc77bc12b738bce8!8m2!3d35.0149143!4d69.1577501!16s%2Fg%2F119v3sx7d?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "1 March - 31 April",
+        text: "GaGul Ghondi Hill is a stunningly beautiful place located in the west of Parwan province where visitors from all across the country come for picnic and to see the blossom of flowers called Arghawan.l "
+    },
+    {
+        id: 7,
+        img: {
+            src: "https://res.cloudinary.com/ddrzvdb1p/image/upload/f_webp,q_80,c_fit,w_2000/premier-holidays/resorts/lzxus54oferzdz3ghbqp.jpg",
+            alt: "Karabi"
+        },
+        title: "Karabi",
+        country: "Thailand",
+        googleMapsLink: "https://www.google.com/maps/place/%DA%A9%D8%B1%D8%A7%D8%A8%DB%8C%D8%8C+%D8%AA%D8%A7%DB%8C%D9%84%D9%86%D8%AF%E2%80%AD/@8.0745583,99.0132952,9z/data=!3m1!4b1!4m6!3m5!1s0x30518d792d7f9309:0x10223bc2c364c90!8m2!3d8.1191811!4d99.1013498!16zL20vMDFnOW1m?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "30 Sep 2019",
+        text: "Krabi is abundant in scenic beauty with lush tropical greenery, rugged limestone cliffs, meandering caves and a stunning coastline with some of the best beaches in the world."
+    },
+    {
+        id: 8,
+        img: {
+            src: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Band-e-Amir_National_Park-9.jpg/1920px-Band-e-Amir_National_Park-9.jpg",
+            alt: "Band-e Amir "
+        },
+        title: "Band-e Amir ",
+        country: "Afghanistan",
+        googleMapsLink: "https://www.google.com/maps/place/%D9%BE%D8%A7%D8%B1%DA%A9+%D9%85%D9%84%DB%8C+%D8%A8%D9%86%D8%AF+%D8%A7%D9%85%DB%8C%D8%B1%E2%80%AD/@34.8353265,67.1810103,13z/data=!3m1!4b1!4m6!3m5!1s0x3f2d4f2bfcf94275:0xbc2a656ca9d5e72a!8m2!3d34.8424366!4d67.211793!16s%2Fg%2F11bwymr6v5?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "22 May 2009",
+        text: "Band-E-Amir is a naturally created group of lakes with special geological formations and structure, as well as natural and unique beauty. Depth is not known, the color is pure blue. For the attraction of the local and international tourists, Band-E-Amir is important, and is protected as a National Park of Afghanistan. "
+    },
+    {
+        id: 9,
+        img: {
+            src: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/be/d1/69/upper-portion-could-not.jpg?w=1100&h=600&s=1",
+            alt: "Shahr-e Zuhak"
+        },
+        title: "Shahr-e Zuhak ",
+        country: "Afghanistan",
+        googleMapsLink: "https://www.google.com/maps/place/34%C2%B049'35.0%22N+67%C2%B049'18.0%22E/@34.8263889,67.8216667,13z/data=!4m4!3m3!8m2!3d34.8263889!4d67.8216667?hl=en&entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "500 - 600",
+        text: "Shahr-e Zuhak or Zuhak City, also known as The Red City, Ruins of a HUGE city sacked by Ghengis Khan. The architecture is beautiful, the views are unbelievable."
+    },
+    {
+        id: 10,
+        img: {
+            src: "https://th-thumbnailer.cdn-si-edu.com/NaExfGA1op64-UvPUjYE5ZqCefk=/fit-in/1600x0/filters:focal(1471x1061:1472x1062)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/b6/30/b630b48b-7344-4661-9264-186b70531bdc/istock-478831658.jpg",
+            alt: "Taj Mahal"
+        },
+        title: "Taj Mahal",
+        country: "India",
+        googleMapsLink: "https://www.google.com/maps/place/%D8%AA%D8%A7%D8%AC+%D9%85%D8%AD%D9%84%E2%80%AD/@27.1751448,78.0421422,17z/data=!3m1!4b1!4m6!3m5!1s0x39747121d702ff6d:0xdd2ae4803f767dde!8m2!3d27.1751448!4d78.0421422!16zL20vMGw4Y2I?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+        dates: "1631 - 1653",
+        text: "An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage."   
+    }
+
 ]
